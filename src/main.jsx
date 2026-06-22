00081996c7335c10163ea65d193765c3932bd366
sungsoo <142865231+sslee1210@users.noreply.github.com>
2026-06-22 14:34:12 +0900

    fix: ensure final layout css is last stylesheet

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,9 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./styles.css";
 import "./sector-stock-list.css";
-import "./readability-tune.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
