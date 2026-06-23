f51aac2db6e9d3a2c451a6b51905ff27a8afd549
sungsoo <142865231+sslee1210@users.noreply.github.com>
2026-06-23 16:19:18 +0900

    Load compact right panel style

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./styles.css";
 import "./flow-alerts.css";
+import "./right-panel-compact.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
